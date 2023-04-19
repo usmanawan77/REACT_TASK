3c32c4525b9a4d2055cef20028d4164b8fbb24ba
Usman Lateef <66667561+usmanawan77@users.noreply.github.com>
2023-04-20 01:51:27 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,12 +6,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
->>>>>>> 9075aad (first commit)
