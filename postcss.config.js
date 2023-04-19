72e3ae4727c851e4484e3fa512c76430667c9e92
Usman Lateef <66667561+usmanawan77@users.noreply.github.com>
2023-04-20 01:52:39 +0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 9075aad (first commit)
